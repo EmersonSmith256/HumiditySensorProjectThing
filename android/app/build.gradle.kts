33baf60db7819a8a5ea3fa5431020050ae402d54
aqua <mraquadude0@gmail.com>
2025-12-29 16:14:16 -0600

    Fixed Android Compiling Issues and swapped to new Firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,12 +3,12 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.android.application")
+    // id("com.android.application")
     id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.humiditysensorproject"
+    namespace = "com.swdclub.iotsensors"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.humiditysensorproject"
+        applicationId = "com.swdclub.iotsensors"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/humiditysensorproject/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/humiditysensorproject/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.humiditysensorproject
+package com.swdclub.iotsensors
 
 import io.flutter.embedding.android.FlutterActivity
 
